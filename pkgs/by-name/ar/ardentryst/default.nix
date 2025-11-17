c14984380442dc3c7b553d0b875f0e82c7446004
Nathan Mills <38995150+Quipyowert2@users.noreply.github.com>
2025-11-17 09:11:23 +0000

    Fix ardentryst.
    The game was not working because Python was not being wrapped.

--- pkgs/by-name/ar/ardentryst/default.nix
+++ pkgs/by-name/ar/ardentryst/default.nix
@@ -1,13 +1,4 @@
+{ pkgs ? import <nixpkgs> {} }:
 {
-  lib,
-  pythonPackages
-}:
-
-pythonPackages.buildPythonModule {
-  pname = "ardentryst";
-  version = "1.71";
-  src = fetchurl {
-     url = "https://github.com/ardentryst/ardentryst/archive/refs/tags/1.71.tar.gz";
-     hash = "";
-  };
+ ardentryst = pkgs.callPackage ./package.nix { };
 }
--- pkgs/by-name/ar/ardentryst/package.nix
+++ pkgs/by-name/ar/ardentryst/package.nix
@@ -6,12 +6,12 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "ardentryst";
-  version = "1.71";
+  version = "1.71-48-g7b66716";
   src = fetchFromGitHub {
     owner = "ardentryst";
     repo = "ardentryst";
     rev = version;
-    hash = "sha256-hr8CKMAOyIi5ICwDYwoGOZMBHygbbvLOmmPZ5sFx2I4=";
+    hash = "sha256-+7L4eWxTCKdWSBdb70kVd2M6uTL6CVmcnZ0MluLiYyQ=";
   };
 
   format = "other";
@@ -20,14 +20,22 @@ python3Packages.buildPythonApplication rec {
     pygame
   ];
 
+  patchPhase = ''
+    sed -i 's!/usr/bin/python!/usr/bin/env python!' ardentryst
+    sed -i 's!/usr/share/games!${placeholder "out"}/share/games!' ardentryst
+  '';
+
   installPhase = ''
     mkdir -p ${placeholder "out"}/share/pixmaps
     cp icon.png ${placeholder "out"}/share/pixmaps/ardentryst.png
     mkdir ${placeholder "out"}/bin
     cp ardentryst ${placeholder "out"}/bin/
-    mkdir -p ${placeholder "out"}/games/ardentryst
-    cp -R * ${placeholder "out"}/games/ardentryst/
+    mkdir -p ${placeholder "out"}/share/games/ardentryst
+    cp -R * ${placeholder "out"}/share/games/ardentryst/
     chmod 0755 ${placeholder "out"}/bin/ardentryst
+    makeWrapper ${python3Packages.python.interpreter} $out/bin/ardentryst \
+      --set PYTHONPATH "$PYTHONPATH:$out/share/games/ardentryst" \
+      --add-flags "$out/share/games/ardentryst/ardentryst.py"
   '';
 
   meta = with lib; {
