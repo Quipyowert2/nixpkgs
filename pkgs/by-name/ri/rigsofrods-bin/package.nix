69dba6a9cb441daec46f0d533280489d90d406cb
Nathan Mills <38995150+Quipyowert2@users.noreply.github.com>
2026-04-11 22:11:21 -0700

    rigsofrods-bin: 2022.12 -> 2026.01

--- pkgs/by-name/ri/rigsofrods-bin/package.nix
+++ pkgs/by-name/ri/rigsofrods-bin/package.nix
@@ -4,41 +4,72 @@
   fetchurl,
   autoPatchelfHook,
   makeWrapper,
+  cmake,
   unzip,
   libGL,
   libice,
   libsm,
   libx11,
   libxrandr,
+  ois,
+  pkg-config,
   zlib,
   alsa-lib,
+  ogre_11,
+  mygui,
+  fmt,
+  rapidjson,
+  angelscript,
+  openal
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "rigsofrods-bin";
-  version = "2022.12";
+  version = "2026.01";
 
   src = fetchurl {
-    url = "https://update.rigsofrods.org/rigs-of-rods-linux-2022-12.zip";
-    hash = "sha256-jj152fd4YHlU6YCVCnN6DKRfmi5+ORpMQVDacy/TPeE=";
+    url = "https://github.com/RigsOfRods/rigs-of-rods/archive/refs/tags/${version}.tar.gz";
+    hash = "sha256-4ZGsh42QXYmMgrw2EO6BGpvGPbdviDor8pS3TqN2fIE=";
   };
 
-  sourceRoot = ".";
+  # Fix OIS include dir not being found.
+  patchPhase = ''
+     sed -i 's/PATH_SUFFIXES OIS/PATH_SUFFIXES ois/' cmake/find-modules/FindOIS.cmake
+  '';
 
   nativeBuildInputs = [
     autoPatchelfHook
     makeWrapper
     unzip
+    cmake
   ];
 
   buildInputs = [
     libGL
+    ogre_11
+    ois
+    openal
+    # Override mygui to enable Ogre support and link it to our ogre_11
+    (mygui.override {
+      withOgre = true;
+      ogre = ogre_11;
+    })
+    fmt
     libice
     libsm
     libx11
     libxrandr
     stdenv.cc.cc
     zlib
+    rapidjson
+    (angelscript.overrideAttrs (oldAttrs: {
+      NIX_CFLAGS_COMPILE = (oldAttrs.NIX_CFLAGS_COMPILE or "") + " -DAS_DEPRECATED";
+    }))
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeFeature "CMAKE_CXX_FLAGS" "-DAS_DEPRECATED")
+    (lib.cmakeFeature "CMAKE_CXX_STANDARD" "17")
   ];
 
   runtimeDependencies = [
