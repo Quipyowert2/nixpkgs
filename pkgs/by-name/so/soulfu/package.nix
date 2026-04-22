a3e8def58ced93f1c4cdc8f91aafc052d694a5f3
Nathan Mills <38995150+Quipyowert2@users.noreply.github.com>
2026-04-21 23:31:07 -0700

    Drafted a package.nix for SoulFu.

---
+++ pkgs/by-name/so/soulfu/package.nix
@@ -0,0 +1,45 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  pkg-config,
+  makeWrapper,
+  libGL,
+  libjpeg,
+  libvorbis,
+  libogg,
+  SDL2
+}:
+
+stdenv.mkDerivation {
+  pname = "soulfu";
+  version = "latest-14-gcc06bff";
+
+  src = fetchFromGitHub {
+    owner = "soulfu-dev";
+    repo = "soulfu";
+    rev = "cc06bffac24036574bc94d867ada41ffaac6282c";
+    hash = "sha256-6ct2wFO/YUtjYQmXKqiSlJNZ9f0/I//vGRMhr4se4AI=";
+    fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    makeWrapper
+  ];
+
+  buildInputs = [
+    SDL2
+    libGL
+    libjpeg
+    libogg
+    libvorbis
+  ];
+
+  meta = {
+    description = "A 3D action role-playing hack and slash dungeon crawler made by Aaron Bishop, the creator of Egoboo";
+    homepage = "https://aaronbishopgames.com";
+    platforms = lib.platforms.linux ++ lib.platforms.windows;
+    maintainers = [ ];
+  };
+}
